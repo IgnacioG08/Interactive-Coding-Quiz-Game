7a882a8702c79761f1f5f0ef4d986a667812930f
IgnacioG08 <134964013+IgnacioG08@users.noreply.github.com>
2023-07-31 16:32:45 -0700

    README completed

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Interactive-Coding-Quiz-Game
+
+## Description
+This game is made using HTML,CSS, and Javascript leaning more on the interactivity provided by Javascript. It can be used to test yourself against the clock on some coding basics. The game will start on clicking the button and the timer will begin, if user gets a question wrong then 5 seconds will be lost. On completion of the game the Score will be logged with your initials which can be saved for future attempts. If you would like a fresh start just clear your scores.
+[Deployed Github Webpage](https://ignaciog08.github.io/Interactive-Coding-Quiz-Game/)
+
+## Visuals
+
+![alt text](./assets/images/pre-quiz.PNG)
+![alt text](./assets/images/post-quiz.PNG)
+
+
+## Installation
+
+N/A
+
+## Usage
+This can be used simply to test your skills with coding or to be inspected to see a basic Javascript quiz game
+
+
+## Credits
+
+N/A
+
+## License
+
+N/A
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,116 +16,116 @@ var questionEl = document.querySelector("#questionEl")
 
 var questions = [
     {
-        question : "Which tag is accurate when linking JavaScript?" ,
-        answer : ["<a>", "<href>", "<src>", "<link>", "<rel>"] ,
-        correct : "<src>"
+        question: "Which tag is accurate when linking JavaScript?",
+        answer: ["<a>", "<href>", "<src>", "<link>", "<rel>"],
+        correct: "<src>"
     },
     {
-        question : "Where do you place your CSS Hypertext Reference in the HTML file?" ,
-        answer : ["upper <body>", "lower <body>", "querySelector", "<head>", "<header>"] ,
-        correct : "<head>"
+        question: "Where do you place your CSS Hypertext Reference in the HTML file?",
+        answer: ["upper <body>", "lower <body>", "querySelector", "<head>", "<header>"],
+        correct: "<head>"
     },
     {
-        question : "Which is not considered a primitive type?" ,
-        answer : ["string", "bigint", "number", "null", "const"] ,
-        correct : "const"
+        question: "Which is not considered a primitive type?",
+        answer: ["string", "bigint", "number", "null", "const"],
+        correct: "const"
     },
     {
-        question : "What is function scope" ,
-        answer : ["global", "local", "if()", "block", "else if"] ,
-        correct : "local"
+        question: "What is function scope",
+        answer: ["global", "local", "if()", "block", "else if"],
+        correct: "local"
     },
     {
-        question : "Which is a considered a variable that never changes?" ,
-        answer : ["var", "x", "let", "const", "y"] ,
-        correct : "const"
+        question: "Which is a considered a variable that never changes?",
+        answer: ["var", "x", "let", "const", "y"],
+        correct: "const"
     }
 ];
 
-if(!localStorage.getItem("scores")) {
+if (!localStorage.getItem("scores")) {
     localStorage.setItem("scores", "");
 }
 
 function countDown() {
-timerInterval = setInterval (function() {
-timeEl--;
-timerEl.textContent = timeEl
-if(timeEl <= 0) {
-    clearInterval(timerInterval)
-}
+    timerInterval = setInterval(function () {
+        timeEl--;
+        timerEl.textContent = timeEl
+        if (timeEl <= 0) {
+            clearInterval(timerInterval)
+        }
 
 
-}, 1000)
+    }, 1000)
 }
 
 function startGame() {
     q = 0;
     timeEl = 60;
-    document.getElementById("scores").style.display="none"
-countDown();
-displayQuestions();
+    document.getElementById("scores").style.display = "none"
+    countDown();
+    displayQuestions();
 }
 
 function endGame() {
-scoreBoard();
-document.getElementById("gameEnd").style.display="block"
-clearInterval(timerInterval)
+    scoreBoard();
+    document.getElementById("gameEnd").style.display = "block"
+    clearInterval(timerInterval)
 }
 
-function displayQuestions () {
-    if(q >= 5) {
+function displayQuestions() {
+    if (q >= 5) {
         questionEl.textContent = "Congratulations";
         listEl.textContent = "Game is Complete";
 
     } else {
         questionEl.textContent = questions[q].question
         textRemoval()
-        for(i = 0; i < questions[q].answer.length; i++) {
+        for (i = 0; i < questions[q].answer.length; i++) {
             answerBtn = document.createElement("button");
             listEl.appendChild(answerBtn)
             answerBtn.textContent = questions[q].answer[i]
-            answerBtn.addEventListener("click", function(event){
-                if(event.target.textContent === questions[q].correct) {
+            answerBtn.addEventListener("click", function (event) {
+                if (event.target.textContent === questions[q].correct) {
                     score++
                     q++
                     displayQuestions()
                 } else {
-                    timeEl = timeEl - 5; 
+                    timeEl = timeEl - 5;
                 }
-                if(timeEl === 0 || q >= 5) {
+                if (timeEl === 0 || q >= 5) {
                     endGame();
                 }
             })
-            }
         }
-        
     }
 
-function scoreBoard () {
- document.getElementById("scoreEl").innerHTML = "Score: " + timeEl
+}
+
+function scoreBoard() {
+    document.getElementById("scoreEl").innerHTML = "Score: " + timeEl
 }
 
 function textRemoval() {
-    while(listEl.firstChild) {
+    while (listEl.firstChild) {
         listEl.removeChild(listEl.firstChild);
     }
 }
 
 
-startBtnEl.addEventListener("click", function() {
- startGame();
+startBtnEl.addEventListener("click", function () {
+    startGame();
 })
 
-document.getElementById("submit").addEventListener("click", function() {
+document.getElementById("submit").addEventListener("click", function () {
     var initials = document.getElementById("initials").value;
-    var record = initials + " " +  timeEl + "<br>";
+    var record = initials + " " + timeEl + "<br>";
     var records = localStorage.getItem("scores") + record;
     localStorage.setItem("scores", records);
     document.getElementById("scoreboard").innerHTML = records;
     document.getElementById("scores").style.display = "block";
     document.getElementById("gameEnd").style.display = "none";
-   })
-   document.getElementById("clear").addEventListener("click", function() {
+})
+document.getElementById("clear").addEventListener("click", function () {
     localStorage.setItem("scores", "");
     document.getElementById("scoreboard").innerHTML = "";
-   })
+})
